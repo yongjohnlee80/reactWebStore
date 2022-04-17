9c03d22f0e52c0b93efc1e0c30ace396fe313a83
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-04-17 00:52:36 -0700

    category and directory component created

---
+++ src/directory/directory.component.jsx
@@ -0,0 +1,10 @@
+import './directory.styles.scss';
+import { CategoryItem } from '../category-item/category-item.component';
+
+export const Directory = ({ categories }) => (
+    <div className="directory-container">
+        {categories.map((category) => (
+            <CategoryItem key={category.id} category={category} />
+        ))}
+    </div>
+);
