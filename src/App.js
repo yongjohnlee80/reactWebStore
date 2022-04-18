56c30af5592f0859504a4a5d1dd5a3ef5c458197
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-04-18 03:39:13 -0700

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Directory } from "./directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
 import categories from "./categories.json";
 
+import Navigation from "./Navigation/navigation.compoenent";
+import Home from "./routes/home/home.component";
+
+
 const App = () => {
     return (
-      <Directory categories={categories}/>
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home categories={categories} />} />
+            </Route>
+        </Routes>
     );
 };
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,9 @@
+import { Directory } from "../../directory/directory.component";
+
+const Home = ({categories}) => {
+    return (
+      <Directory categories={categories}/>
+    );
+};
+
+export default Home;
