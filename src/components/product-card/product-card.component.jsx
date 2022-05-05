4c6dffb8788928f92046757c51a4135a46f3518b
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-05-05 01:33:15 -0700

    added functionality of the shopping cart with React Context

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,15 @@
 import "./product-card.styles.scss";
 
 import { Button } from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
+import { useContext } from "react";
 
 export const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +17,9 @@ export const ProductCard = ({ product }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-            <Button buttonType="inverted">Add to card</Button>
+            <Button buttonType="inverted" onClick={addProductToCart}>
+                Add to card
+            </Button>
         </div>
     );
 };
