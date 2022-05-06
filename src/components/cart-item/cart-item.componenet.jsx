cf051dc6371f3946b723e401b5ef498a93e875b7
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-05-05 23:12:08 -0700

    added in our add to cart feature

--- src/components/cart-item/cart-item.componenet.jsx
+++ src/components/cart-item/cart-item.componenet.jsx
@@ -1,11 +1,16 @@
-import './cart-item.styles.scss';
+import "./cart-item.styles.scss";
 
-const CartItem = ({cartItem}) => {
-    const {name, quantity } = cartItem;
-    return(
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+const CartItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <div className="cart-item-container">
+            <img src={imageUrl} alt={`${name}`} />
+            <div className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">
+                    {quantity} x ${price}
+                </span>
+            </div>
         </div>
     );
 };
